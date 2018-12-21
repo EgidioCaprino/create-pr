f1468a2ca5541b96b711c463bc95f58b5d19e6ef
Egidio Caprino <egidio.caprino@socialmediaemotions.com>
2018-12-21 15:29:21 +0100

    docs: update readme file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs';
+import { homedir } from 'os';
 
-const string = readFileSync('create-pr.json').toString();
+const string = readFileSync(`${homedir()}/create-pr.json`).toString();
 const object = JSON.parse(string);
 
 export default object;
