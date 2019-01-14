2850d20dd887a4079deeb5d4e9c3aa88d6bc8c63
Egidio Caprino <egidio.caprino@socialmediaemotions.com>
2019-01-14 14:53:26 +0100

    feat: can read config from project directory
    The global configuration file defined in `~/create-pr.json` can be overridden by a `create-pr.json` file in the current working directory (usually the project where the user is creating the PR). The two configuration files are deeply merged.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,14 @@
-import { readFileSync } from 'fs';
+import { existsSync, lstatSync, readFileSync } from 'fs';
 import { homedir } from 'os';
+import merge from 'lodash/merge';
 
-const string = readFileSync(`${homedir()}/create-pr.json`).toString();
-const object = JSON.parse(string);
+const readConfig = (path) => {
+  const valid = existsSync(path) && lstatSync(path).isFile();
+  const string = valid ? readFileSync(path).toString() : '{}';
+  return JSON.parse(string);
+};
 
-export default object;
+const globalConfig = readConfig(`${homedir()}/create-pr.json`);
+const currentConfig = readConfig(`${process.cwd()}/create-pr.json`);
+
+export default merge({}, globalConfig, currentConfig);
